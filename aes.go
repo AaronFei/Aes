2127333d22c08a70ec97ca602e21a0c846165a66
AaronFei <xyzyx760326@gmail.com>
2022-11-02 03:05:32 +0800

    Create aes.go

---
+++ aes.go
@@ -0,0 +1,44 @@
+package aesHandle
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+	"errors"
+)
+
+func Decrypt(key []byte, cryptMsg string) (plainText string, err error) {
+	cipherText, err := base64.URLEncoding.DecodeString(cryptMsg)
+	if err != nil {
+		return
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+	if len(cipherText) < aes.BlockSize {
+		err = errors.New("The conten is too short")
+		return
+	}
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(cipherText, cipherText)
+	plainText = string(cipherText)
+	return
+}
+
+func Encrypt(key []byte, text string) (cryptText string, err error) {
+	plainText := []byte(text)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return
+	}
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+	cryptText = base64.URLEncoding.EncodeToString(cipherText)
+	return
+}
